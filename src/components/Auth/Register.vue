49c2d999f3a8af1f737630ba59be9447cf338f24
antoine-git <antoine.macheda@ynov.com>
2021-01-20 17:37:00 +0100

    add log pass

---
+++ src/components/Auth/Register.vue
@@ -0,0 +1,193 @@
+<template>
+ <div class="relative m-2">
+    <button
+      class="flex justify-center items-center | hover:border-white focus:outline-none
+      focus:border-white"
+      @click="dialog2 = true"
+    >
+      Sign Up
+    </button>
+    <div v-if="dialog2" class="fixed inset-0
+      w-full h-screen z-20 bg-black opacity-25"
+          @click="dialog2 = false"></div>
+    <div class="absolute z-30 right-0 mt-2" :class="{'hidden': !dialog2}">
+      <div class="rounded-lg shadow-lg py-2 form-class">
+          <form @submit.prevent="register" autocomplete="off">
+            <div class="w-full flex flex-wrap">
+              <div class="flex flex-col w-1/2 m-auto">
+                  <div class="flex flex-col w-full m-0">
+                      <div class="flex flex-col pt-4">
+                          <label for="login" class="text-lg">Login</label>
+                          <input
+                            autocomplete="off"
+                            required type="text"
+                            name="login"
+                            id="login"
+                            placeholder=""
+                            v-model="form.login"
+                            class="
+                              shadow
+                              appearance-none
+                              border
+                              rounded
+                              w-full
+                              py-2
+                              px-3
+                              text-gray-700
+                              mt-1
+                              leading-tight
+                              focus:outline-none
+                              focus:shadow-outline"
+                            >
+                      </div>
+                      <div class="flex flex-col pt-4">
+                          <label for="email" class="text-lg">Email</label>
+                          <input
+                            autocomplete="off"
+                            required type="email"
+                            name="email"
+                            id="email"
+                            placeholder=""
+                            v-model="form.email"
+                            class="
+                              shadow
+                              appearance-none
+                              border
+                              rounded
+                              w-full
+                              py-2
+                              px-3
+                              text-gray-700
+                              mt-1
+                              leading-tight
+                              focus:outline-none
+                              focus:shadow-outline"
+                            >
+                      </div>
+                      <div class="flex flex-col pt-4">
+                          <label for="password" class="text-lg">Password</label>
+                          <div class="flex">
+                            <input
+                              autocomplete="off"
+                              required name="password"
+                              :type="passwordVisible ? 'text' : 'password'"
+                              id="password"
+                              placeholder=""
+                              v-model="form.password"
+                              class="
+                                shadow
+                                appearance-none
+                                border
+                                rounded
+                                flex-1
+                                py-2
+                                px-3
+                                text-gray-700
+                                mt-1
+                                leading-tight
+                                focus:outline-none
+                                focus:shadow-outline"
+                              >
+                            <button
+                              type="button"
+                              @click="togglePasswordVisibility"
+                              id="view-password"
+                              class="
+                                appearance-none
+                                border-none
+                                rounded
+                                py-2
+                                px-3
+                                mt-1
+                                leading-tight
+                                focus:outline-none
+                                focus:shadow-outline"
+                              >
+                              <v-mdi
+                                :name="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                                height="20" width="20"/>
+                            </button>
+                          </div>
+                      </div>
+                      <div class="flex align-center pt-4 mt-8">
+                        <input
+                          type='submit'
+                          :disabled="isLoading == true"
+                          value="Sign Up"
+                          class="
+                            button
+                            bg-black
+                            text-white
+                            font-bold
+                            text-lg
+                            hover:bg-gray-700
+                            flex-1"
+                          >
+                        <div
+                          :style="isLoading ? 'display: block' : 'display: none'"
+                          id="is-loading"
+                          class="
+                            py-2
+                            px-3
+                            mt-1"
+                          >
+                          <i class="gg-spinner-two"></i>
+                        </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      dialog2: false,
+      form: {
+        login: '',
+        email: '',
+        password: '',
+      },
+      passwordVisible: false,
+      isLoading: false,
+    };
+  },
+  methods: {
+    togglePasswordVisibility() {
+      this.passwordVisible = !this.passwordVisible;
+    },
+    ...mapActions({
+      signUp: 'auth/signUp',
+    }),
+    register() {
+      this.isLoading = true;
+      this.signUp(this.form).then((data) => {
+        this.isLoading = false;
+        if (data.token) {
+          localStorage.setItem('token', data.token);
+          this.$store.commit('changeState', {
+            key: 'token',
+            data: data.token,
+          });
+          localStorage.setItem('isLogged', true);
+          window.location.href = '/'; // FIX Mauvaise méthode je pense mais ça marche
+        }
+      });
+    },
+  },
+};
+/* eslint-disable eol-last */
+</script>
+<style scoped>
+ .form-class{
+    width: 18rem;
+    border: 1px whitesmoke solid;
+  }
+</style>
--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,66 +1,79 @@
 <template>
- <div class="flex justify-center w-full items-center">
-    <div class="w-full max-w-xs">
-      <form class="shadow-md rounded
-      px-10 pt-6 pb-8 mb-4 bg-purple-600">
-        <div class="mb-4">
-          <label class="block text-purple-700 text-sm font-bold mb-2" for="lastname">
-            Nom
-          </label>
-          <input class="shadow appearance-none border rounded
-          w-full py-2 px-3 text-purple-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Nom">
-        </div>
-        <div class="mb-4">
-          <label class="block text-purple-700 text-sm font-bold mb-2" for="name">
-            Prénom
-          </label>
-          <input class="shadow appearance-none border rounded
-          w-full py-2 px-3 text-purple-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Prénom">
-        </div>
-        <div class="mb-4">
-          <label class="block text-purple-700 text-sm font-bold mb-2" for="username">
-            Nom D'utilisateur
-          </label>
-          <input class="shadow appearance-none border rounded
-          w-full py-2 px-3 text-purple-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Nom D'Utilisateur">
-        </div>
-        <div class="mb-6">
-          <label class="block text-purple-700 text-sm font-bold mb-2" for="password">
-            Mot de passe
-          </label>
-          <input class="shadow appearance-none border rounded w-full py-2
-          px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password" type="password" placeholder="Mot de passe">
-        </div>
-        <div class="flex items-center justify-between">
-          <button class="bg-purple-500 hover:bg-blue-700 text-white
-          font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-            Sign In
-          </button>
-          <a class='
-            inline-block
-            align-baseline
-            font-bold
-            text-sm text-purple-500
-            hover:text-blue-800'
-          href="#">
-            Identifiants oubliés ?
-          </a>
-        </div>
-      </form>
-    </div>
-  </div>
+ <div class="w-full">
+   <base-nav-bar/>
+   <section id="sec-1">
+     <div class="container">
+       <div class="left">
+         <h1>Code Snippet</h1>
+         <h2>Pourquoi ne pas essaye de reprendre le code en collaboration avec ses eleves</h2>
+         <p>testetstetestetstetes
+           tetstetestetstetestetstetestets
+           tetestetstetestetstetestetsteteste
+           tstetestetstetestetste</p>
+       </div>
+       <div class="right">
+         <div class="img-mid">
+           <img src="https://picsum.photos/500" alt="trest"/>
+       </div>
+       </div>
+     </div>
+   </section>
+   <section id="sec-2">
+     <div class="container">
+       <div class="team">
+         <h1 class="team-h1">La fine Equipe</h1>
+         <!-- member-->
+         <div class="team-member">
+           <img class="team-photo" src="http://square-vn.com/app/dscms/assets/images/person-4.jpg?v=1495618120">
+           <h3>Person's Name</h3>
+           <span>
+             This section could be used to show a very short bio description for each member</span>
+         </div>
+         <!-- member-->
+         <div class="team-member">
+           <img class="team-photo" src="http://square-vn.com/app/dscms/assets/images/person-4.jpg?v=1495618120">
+           <h3>Person's Name</h3>
+           <span>
+             This section could be used to show a very short bio description for each member</span>
+         </div>
+         <!-- member-->
+         <div class="team-member">
+           <img class="team-photo" src="http://square-vn.com/app/dscms/assets/images/person-4.jpg?v=1495618120">
+           <h3>Person's Name</h3>
+           <span>
+             This section could be used to show a very short bio description for each member</span>
+         </div>
+         <!-- member-->
+         <div class="team-member">
+           <img class="team-photo" src="http://square-vn.com/app/dscms/assets/images/person-4.jpg?v=1495618120">
+           <h3>Person's Name</h3>
+           <span>
+             This section could be used to show a very short bio description for each member</span>
+         </div>
+         <!-- member-->
+         <div class="team-member">
+           <img class="team-photo" src="http://square-vn.com/app/dscms/assets/images/person-4.jpg?v=1495618120">
+           <h3>Person's Name</h3>
+           <span>
+             This section could be used to show a very short bio description for each member</span>
+         </div>
+       </div>
+     </div>
+   </section>
+ </div>
 </template>
 <script>
+import BaseNavBar from '../components/Base/BaseNavBar.vue';
 
 export default {
+  components: { BaseNavBar },
   data: () => ({
     retrieved: false,
     windowSize: 0,
     counter: 0,
+    email: '',
+    userName: '',
+    password: '',
   }),
   created() {
     this.$store.dispatch('retrieve').then(({ dark }) => {
@@ -77,7 +90,7 @@ export default {
     resizeHandler() {
       this.windowSize = window.innerWidth;
     },
-    counnt() {
+    count() {
       this.counter += 1;
     },
   },
@@ -89,5 +102,143 @@ export default {
 </script>
 
 <style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: monospace;
+}
+
+.container {
+  width: 95%;
+  height: 100%;
+  margin: 0 auto;
+}
+
+section#sec-1 {
+  height: 80vh;
+}
+
+section#sec-1 h1 {
+  font-size: 46px;
+}
+
+section#sec-1 h2 {
+  font-size: 25px;
+  font-weight: 500;
+  margin: 25px 0;
+  max-width: 600px;
+}
+
+section#sec-1 p {
+  font-size: 12px;
+  color: grey;
+  max-width: 450px;
+  padding-left: 7px;
+}
+
+section#sec-1 .container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+section#sec-1 .right img {
+  padding: 50px 0;
+  width: 400px;
+  border-radius: 15%;
+}
+.right{
+  position: relative;
+  animation-timing-function: ease-in;
+  animation: mymove 2s;
+}
+.left{
+  position: relative;
+  animation-timing-function: ease-in;
+  animation: mymove-2 2s;
+}
+
+@keyframes mymove {
+  from { left: 500px; top:100px;}
+  to { left: 0px; top:0px}
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+@keyframes mymove-2 {
+  from { right: 500px; bottom:100px;}
+  to { right: 0px; bottom:0px}
+  0% {opacity:0;}
+  100% {opacity:1;}
+}
+
+section#sec-2 {
+  height: 100vh;
+}
+section#sec-2 .container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+section#sec-2 .team-h1 {
+  display: flex;
+  justify-content: center;
+  padding: 25px;
+}
+section#sec-2 .team-container {
+  margin: auto;
+  border-top: 1px #e9e9e9 solid;
+  border-bottom: 1px #e9e9e9 solid;
+  padding-top: 5em;
+  padding-bottom: 5em;
+  margin-top: 3em;
+}
+section#sec-2 .team-member {
+  width: 16%;
+  float: left;
+  text-align: center;
+  margin-right: 5%;
+}
+section#sec-2 .team-member:last-child { margin-right: 0; }
+
+section#sec-2 .team-member span,
+.team-member h3 {
+  max-width: 200px;
+  font-family: sans-serif;
+  letter-spacing: -1px;
+}
+section#sec-2 .team-member h3 {
+  color: #1CA3A3;
+}
+
+section#sec-2 .team-photo {
+  border-radius: 50%;
+  text-align: center;
+  margin: auto;
+  max-width: 100%;
+  height: auto;
+  transition: 0.5s transform ease-in-out;
+}
+section#sec-2.team-photo:hover {
+  transform: scale(0.9);
+  cursor: pointer;
+}
+/* code for phone layout */
+@media (max-width:850px){
+  .team-member {
+    width: 25%;
+    margin-left: 4%;
+    margin-right: 4%;
+    margin-bottom: 40px;
+
+  }
+  section#sec-1 .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  section#sec-1 img{
+    width: 100%;
+  }
+}
 
 </style>
