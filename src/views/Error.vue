c3aa75296b32c14c883fa4ed29846ed7f707fb0c
Aimee <aimeeritleng@outlook.com>
2021-02-05 19:06:12 +0100

    ADD Error Pages

---
+++ src/views/Error.vue
@@ -0,0 +1,50 @@
+<template>
+  <main class="area w-full">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <div id="errorPage">
+          <div class="flex items-center justify-center mt5">
+            <router-view />
+            <div class="w-1/4">
+                <div class="error-img-container">
+                <img class="rounded-lg" :src="this.images[this.randomIndex].pathLong" alt="😺" />
+                </div>
+            </div>
+          </div>
+      </div>
+  </main>
+</template>
+
+<script>
+import '~/assets/scss/pages/_error.scss';
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+      images: [],
+      randomIndex: Math.floor(Math.random() * 2),
+    };
+  },
+  mounted() {
+    this.importAll(require.context('../assets/img/randomCats/', true, /\.jpg$/));
+  },
+  methods: {
+    importAll(r) {
+      r.keys().forEach((key) => (this.images.push({ pathLong: r(key), pathShort: key })));
+    },
+  },
+};
+</script>
--- src/views/Snippets.vue
+++ src/views/Snippets.vue
@@ -12,43 +12,8 @@
           <home-header
             :file="file"
             :hide="!isFileActive"></home-header>
-          <file-tags
-            v-if="isFileActive"
-            :folder-id="file.folderId"
-            :file-id="file.id"
-            :tags="file.tags"></file-tags>
         </div>
-        <template v-if="isFileActive">
-          <vue-codemirror
-            @cursorPosition="cursor = $event"
-            :file="file"
-            class="flex-auto overflow-y-auto px-4"></vue-codemirror>
-          <div class="flex items-center py-1 pb-2 px-6 text-sm">
-            <v-popover placement="top-end">
-              <p class="cursor-pointer">{{ getMimeName(file.mode) }}</p>
-              <card-ui class="shadow-xl border" slot="popover">
-                <list-ui
-                  v-for="mode in modes"
-                  :key="mode.mime"
-                  :active="mode.mime === file.mode"
-                  class="mb-2"
-                  v-close-popover
-                  @click="changeMode(mode.mime)">{{ mode.name }}</list-ui>
-              </card-ui>
-            </v-popover>
-            <div class="flex-grow"></div>
-            <span>
-         Line {{ cursor.line + 1 }}, Column {{ cursor.column + 1 }}
-        </span>
-          </div>
-        </template>
-        <template v-else>
-          <empty-state-ui
-            class="mt-10"
-            icon="mdi-file"
-            title="Select snippet to view content">
-          </empty-state-ui>
-        </template>
+        <router-view />
       </div>
     </main>
   </div>
@@ -60,12 +25,12 @@ import SideMenu from '~/components/Layout/SideMenu.vue';
 import MobileMenu from '~/components/Layout/MobileMenu.vue';
 import Files from '~/components/Layout/Files.vue';
 import HomeHeader from '~/components/Pages/Home/Header.vue';
-import FileTags from '~/components/Pages/Home/FileTags.vue';
-import VueCodemirror from '~/components/Pages/Home/VueCodemirror.vue';
+// import VueCodemirror from '~/components/Pages/Home/VueCodemirror.vue';
 
 export default {
   components: {
-    SideMenu, Files, MobileMenu, HomeHeader, FileTags, VueCodemirror,
+    SideMenu, Files, MobileMenu, HomeHeader,
+    // VueCodemirror,
   },
   name: 'Snippets',
   data: () => ({
