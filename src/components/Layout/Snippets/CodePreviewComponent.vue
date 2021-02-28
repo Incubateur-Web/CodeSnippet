140732568ef2b04a1f6f9dcc41efdcaf27341fd6
Sanchez <gregg.sanchez.pro@gmail.com>
2021-02-28 15:18:47 +0100

    refacto : separate into component
    CodeEditingComponent
    CodePreviewComponent

---
+++ src/components/Layout/Snippets/CodePreviewComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="preview" class="shadow w-full bg-card">
+    <div class="h-full flex items-center justify-center">
+      <h2 class="uppercase font-bold">preview</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodePreviewComponent',
+};
+</script>
+
+<style scoped>
+
+</style>
